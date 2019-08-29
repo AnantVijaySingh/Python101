0a622a02e0965dfb73fa87b808504d5d89038be2
Anant Vijay <anantvijay.23@gmail.com>
2019-08-28 22:35:46 -0700

    Data types and string methods

--- variables_dataStructures.py
+++ variables_dataStructures.py
@@ -49,3 +49,65 @@ print(float(2 + 1))  # Converts to float
 print(.1 + .1 + .1 == .3)  # This is false due to the approximation that Python applies
 
 
+# ---------------------------------------------------------------------------
+# ---------------------------------------------------------------------------
+# ---------------------------------------------------------------------------
+
+# Strings
+udacity_length = len('udacity')
+print(udacity_length)
+print(type('string'))
+
+this_string = 'Simon\'s skateboard is in the garage.'  #using delimiters to support ' / " in strings
+print(this_string)
+
+# Strings are arrays and can be accessed with  an argument
+first_world = 'Hello World'
+print(first_world[0])
+
+# ---------------------------------------------------------------------------
+# ---------------------------------------------------------------------------
+# ---------------------------------------------------------------------------
+# Data types and conversion
+
+mon_sales = "121"
+tues_sales = "105"
+wed_sales = "110"
+thurs_sales = "98"
+fri_sales = "95"
+
+#TODO: Print a string with this format: This week's total sales: xxx
+# You will probably need to write some lines of code before the print statement.
+
+total_sales = int(mon_sales) + int(tues_sales) + int(wed_sales) + int(thurs_sales) + int(fri_sales)  # converting string to int
+print("This week's total sales: " + str(total_sales))
+
+# ---------------------------------------------------------------------------
+# ---------------------------------------------------------------------------
+# ---------------------------------------------------------------------------
+# String methods
+# Print, int, type are functions
+# .isLower , .count are methods. Methods work on objects and thus the first argument is the object itself.
+# Methods can contain more arguments such as .count(args).
+
+my_string = 'Learning  Python from Udacity'
+print(my_string.islower())
+print(my_string.count('a'))
+print(my_string.find('P'))
+
+print("Beckham has {} balloons".format(27))
+
+animal = "dog"
+action = "bite"
+print("Does your {} {}?".format(animal, action))
+
+maria_string = "Maria loves {1} and {0}"
+print(maria_string.format("math", "statistics"))
+
+"First, thou shalt count to {0}"  # References first positional argument
+"Bring me a {}"                   # Implicitly references the first positional argument
+"From {} to {}"                   # Same as "From {0} to {1}"
+"My quest is {name}"              # References keyword argument 'name'
+"Weight in tons {0.weight}"       # 'weight' attribute of first positional arg
+"Units destroyed: {players[0]}"   # First element of keyword argument 'players'.
+
